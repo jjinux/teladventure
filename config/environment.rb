a8257180d28d3ede23e7fc46c785d33f56535573
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-19 14:49:03 -0700

    Require nokogiri.

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "foreigner"
   config.gem "erubis", :version => ">=2.6.5"
+  config.gem "nokogiri", :version => ">=1.4.2"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
