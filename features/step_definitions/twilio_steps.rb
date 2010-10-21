4a42fd7c74d472f09a502b9ed7a99be0a058f0a0
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-21 11:28:45 -0700

    Follow the redirect.  Introduce the node.

--- features/step_definitions/twilio_steps.rb
+++ features/step_definitions/twilio_steps.rb
@@ -7,7 +7,22 @@ When /^I receive a phone call$/ do
   get url_for(:controller => :twilio)
 end
 
-Then /^it should say hello$/ do
+Then /^it should introduce me to the game$/ do
   assert_valid_xml_document
-  @doc.xpath("/Response/Say")[0].content.should == "Hi"
+  @doc.xpath("/Response/Say").first.content.should == "Hello."
+end
+
+When /^I follow the redirect$/ do
+  redirect = @doc.xpath("/Response/Redirect").first
+  redirect.should_not be_nil
+
+  method = redirect[:method].to_s || "POST"
+  %w(GET POST).should include(method)
+
+  request_via_redirect(method.downcase, redirect.content)
+end
+
+Then /^it should introduce me to the node$/ do
+  assert_valid_xml_document
+  @doc.xpath("/Response/Play").first.should_not be_nil
 end
