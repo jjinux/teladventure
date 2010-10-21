6bf0a39e01f98181968a1882db72afe0f091e720
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-21 13:05:23 -0700

    Find the node to show to the user.

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,9 +1,17 @@
 class TwilioController < ApplicationController
+  before_filter :find_node, :only => [:introduce_node]
+
   def index
     render_xml
   end
 
   def introduce_node
     render_xml
+  end
+
+  private
+
+  def find_node
+    @node = params[:id] ? Node.find(params[:id]) : Node.root
   end
 end
