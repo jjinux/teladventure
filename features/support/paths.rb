86005c312de672d3ccf97ff0f17f1bc4618aa31b
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-22 09:46:18 -0700

    Added a path for the root node.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,9 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /the root node/
+      url_for :controller => :twilio, :action => :show_node
+
     else
       begin
         page_name =~ /the (.*) page/
