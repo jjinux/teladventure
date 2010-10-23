2f3b73cebb91e96b799b980c8edee721bd353fbd
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-22 18:05:03 -0700

    Changed the way choices work.

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,4 +1,7 @@
 class TwilioController < ApplicationController
+  # This breaks Twilio applications.
+  before_filter :verify_authenticity_token, :only => []
+
   before_filter :find_node, :only => [:show_node]
   helper_method :give_user_choices
 
@@ -11,6 +14,7 @@ class TwilioController < ApplicationController
 
     @node.children.each_with_index do |child, i|
       @choices << Choice.new(
+        label("show_node(#{i + 1})"),
         digits(i + 1),
         view_block { @xml.Play(child.choice) },
         controller_block { redirect_to :action => :show_node, :id => child.id }
@@ -19,12 +23,14 @@ class TwilioController < ApplicationController
 
     i = 0
     @choices << Choice.new(
+      label(:create_node),
       digits("*#{i += 1}"),
       view_block { @xml.Say("create a new choice and outcome.") },
       controller_block { redirect_to :action => :create_node, :parent_id => @node.id }
     )
 
     @choices << Choice.new(
+      label(:edit_node),
       digits("*#{i += 1}"),
       view_block { @xml.Say("edit the current choice and outcome.") },
       controller_block { redirect_to :action => :edit_node, :id => @node.id }
@@ -32,6 +38,7 @@ class TwilioController < ApplicationController
 
     if @node.parent
       @choices << Choice.new(
+        label("show_node(#{@node.parent_id})"),
         digits("*#{i += 1}"),
         view_block { @xml.Say("go back a step.") },
         controller_block { redirect_to :action => :show_node, :id => @node.parent_id }
@@ -39,14 +46,7 @@ class TwilioController < ApplicationController
     end
 
     if request.post?
-      if params[:Digits]
-        @choices[params[:Digits].to_i].controller_block.call
-        # XXX Do more.
-      else
-        @error = "I'm sorry.  I didn't get a response.  Let's try again."
-        @redirect = url_for
-        render_xml(:action => :error)
-      end
+      handle_choice
     else
       render_xml
     end
@@ -60,10 +60,15 @@ class TwilioController < ApplicationController
 
   # This is a little DSL for choices:
   #
-  #   Choice.new(digits("*2"),
+  #   Choice.new(label(:action_name),
+  #              digits("*2"),
   #              view_block { @xml... },
   #              controller_block { redirect_to... })
-  Choice = Struct.new(:digits, :view_block, :controller_block)
+  Choice = Struct.new(:label, :digits, :view_block, :controller_block)
+
+  def label(value)
+    value.to_s
+  end
 
   def digits(value)
     value.to_s
@@ -81,5 +86,24 @@ class TwilioController < ApplicationController
       @xml.Say("Press #{choice.digits} if you would like to ")
       choice.view_block.call
     end
+  end
+
+  # Respond to the user's choice.
+  def handle_choice
+    digits = params[:Digits]
+    unless digits
+      @error = "I'm sorry.  I didn't get a response.  Let's try again."
+      @redirect = url_for
+      return render_xml(:action => :error)
+    end
+
+    choice = @choices.find { |c| [c.label, c.digits].include?(digits) }
+    unless choice
+      @error = "#{digits} is not a valid entry.  Let's try again."
+      @redirect = url_for
+      return render_xml(:action => :error)
+    end
+
+    choice.controller_block.call
   end
 end
