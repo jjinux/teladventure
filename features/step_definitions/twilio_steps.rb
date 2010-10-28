c9a94cda5a6ef8888b1269abf891f702ade55470
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-27 21:29:31 -0700

    Added a bunch of generally useful steps for working with Twilio.

--- features/step_definitions/twilio_steps.rb
+++ features/step_definitions/twilio_steps.rb
@@ -1,25 +1,54 @@
-Given /^there are a few nodes$/ do
-  Node.create_a_few_nodes
-end
-
-When /^I receive a phone call$/ do
-  get url_for(:controller => :twilio)
+# Return "get" or "post" based on method which may be nil.
+#
+# "post" is the default.
+def sanitize_method(method)
+  method = (method || "post").to_s.downcase
+  %w(get post).should include(method)
+  method
 end
 
 When /^I follow the redirect$/ do
   redirect = @doc.xpath("/Response/Redirect").first
   redirect.should_not be_nil
+  request_via_redirect(sanitize_method(redirect[:method]), redirect.content)
+end
 
-  method = redirect[:method].to_s || "POST"
-  %w(GET POST).should include(method)
+When /^I enter "([^"]*)"$/ do |digits|
+  gather = @doc.xpath("/Response/Gather").first
+  gather.should_not be_nil
+  request_via_redirect(sanitize_method(gather[:method]), gather[:action], {:Digits => digits})
+end
 
-  request_via_redirect(method.downcase, redirect.content)
+When /^I record something with the URL "([^"]*)"$/ do |url|
+  record = @doc.xpath("/Response/Record").first
+  record.should_not be_nil
+  request_via_redirect(sanitize_method(record[:method]), record[:action], {:RecordingUrl => url})
 end
 
 Then /^I should get a valid TwiML response$/ do
   assert_response :success
   @doc = Nokogiri::XML(response.body)
   @doc.xpath("/Response").size.should == 1
+end
+
+Then /^it should (not |)(say|play) "([^"]*)"$/ do |not_present, verb, msg|
+  @doc.xpath("//#{verb.titlecase}").any? { |e| e.content.include?(msg) }.should == (not_present == "")
+end
+
+Then /^it should record something$/ do
+  @doc.xpath("/Response/Record").should_not be_empty
+end
+
+Then /^it should redirect me if I time out$/ do
+  @doc.xpath("/Response/Redirect").should_not be_empty
+end
+
+Given /^there are a few nodes$/ do
+  Node.create_a_few_nodes
+end
+
+When /^I receive a phone call$/ do
+  get url_for(:controller => :twilio)
 end
 
 Then /^it should tell me the current outcome$/ do
@@ -31,14 +60,6 @@ Then /^it should ask me for the next choice$/ do
 end
 
 Then /^it should redirect me to the current node if I haven't made a choice$/ do
-  @doc.xpath("/Response/Redirect").should_not be_empty
-end
-
-Then /^it should (say|play) "([^"]*)"$/ do |verb, msg|
-  @doc.xpath("//#{verb.titlecase}").any? { |e| e.content.include?(msg) }.should == true
-end
-
-Then /^it should redirect me if I time out$/ do
   @doc.xpath("/Response/Redirect").should_not be_empty
 end
 
