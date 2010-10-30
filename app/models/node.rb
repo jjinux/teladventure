10123406f3451049dab9ff34711dd9c38d97ac2b
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-29 22:00:12 -0700

    Refactored for reuse.

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,17 +2,20 @@ class Node < ActiveRecord::Base
   acts_as_tree :order => "created_at"
   attr_accessible :choice, :outcome
 
-  # Delete all the nodes and create a few for testing or development.
-  def self.create_a_few_nodes
-    Node.delete_all
-
+  def self.create_a_root_node
     # It's been a good morning so far.  You woke up before your alarm.  You got in a
     # shower and even managed to find something to eat.  Something inside you tells
     # you this is going to be a great day.  You exit the front door and start
     # singing "I Feel Lucky".  As you close the door, a mysterious, long black
     # limo pulls up to the curb.  The door opens, and a leg confidently steps
     # out onto the curb.
-    root = Node.create!(:outcome => "http://api.twilio.com/2010-04-01/Accounts/ACec5bb8f63c52532cb3a8c18a1b2e85b1/Recordings/RE48c9b4391d0850546843da3d1c4f1070")
+    Node.create!(:outcome => "http://api.twilio.com/2010-04-01/Accounts/ACec5bb8f63c52532cb3a8c18a1b2e85b1/Recordings/RE48c9b4391d0850546843da3d1c4f1070")
+  end
+
+  # Delete all the nodes and create a few for testing or development.
+  def self.create_a_few_nodes
+    Node.delete_all
+    root = create_a_root_node
 
     # Ignore the limo and walk nonchalantly toward your car.
     #
